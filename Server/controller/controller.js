912514fe3f895fadc51fdb2b2aa85a587853732e
Morel-D <morelDen@gmail.com>
2022-11-04 14:45:42 -0700

    CRUD operation on server side with controller class

---
+++ Server/controller/controller.js
@@ -0,0 +1,85 @@
+const records = require('../model/model');
+const mongoose = require('mongoose');
+
+
+// POST date (CREATE)
+const getData = async (req, res) =>
+{
+    const record = new records(req.body)
+    record.save();
+
+    records.find()
+        .then((results) => {
+        res.json(results)
+    }).catch((error) => {
+        console.log('Something went wrong', error)
+    })
+}   
+
+
+// // GET all data (READ)
+const getAllData = (req, res) => {
+    records.find().sort({ createdAt: -1 })
+        .then((results) => {
+        res.json(results)
+        }).catch((error) => {
+        console.log('Something went wrong', error)
+    })
+}
+
+
+// GET single data
+
+const getSingleData = (req, res) => {
+
+    const id = mongoose.Types.ObjectId(req.params.id);
+    records.findById(id)
+        .then((results) => {
+            res.json(results)
+            
+    }).catch((error) => {
+        console.log('Something went wrong', error)
+    })
+}
+
+
+// DELETE single data
+
+const deleteData = (req, res) => {
+
+    const id = mongoose.Types.ObjectId(req.params.id);
+
+    records.findByIdAndRemove(id)
+        .then(results => {
+        res.json(results)
+        }).catch(error => {
+            res.json({ err: " Something went wrong" })
+            console.log(error)
+    })
+} 
+
+
+const updateData = (req, res) => {
+    
+    const id = mongoose.Types.ObjectId(req.params.id);
+
+    const newRecord = {
+        userName: req.body.userName,
+        workTitle: req.body.workTitle,
+        startDate: req.body.startDate,
+        finishDate: req.body.finishDate,
+        comment: req.body.comment
+    }
+
+    records.findByIdAndUpdate(id, newRecord)
+        .then(results => {
+        res.json(results)
+        }).catch(error => {
+            res.json({ err: "Something went wrong" })
+            console.log(error)
+    })
+
+}
+
+
+module.exports = { getData, getAllData, getSingleData, deleteData, updateData }
--- Server/routers/routesPath.js
+++ Server/routers/routesPath.js
@@ -1,41 +1,24 @@
 const express = require('express');
 const routers = express.Router();
-const records = require('../model/model');
+const controller = require('../controller/controller')
+
 
 // CRUD Opertaion
 
 // GET all data (READ)
-routers.get('/', (req, res) => {
-    res.json({msg: 'Get all data'})
-})
+routers.get('/', controller.getAllData)
 
 // POST date (CREATE)
-routers.post('/', async (req, res) => {
-
-    const { userName, workTitle, startDate, finishDate } = req.body;
-
-    try {
-        const report = await records.create({ userName, workTitle, startDate, finishDate });
-        res.status(200).json(report);
-    } catch(error) {
-        res.status(400).json({error: error.message})
-    }
-})
+routers.post('/', controller.getData);
 
 // GET single data
-routers.get('/:id', (req, res) => {
-    res.json({msg : "Get single data"})
-})
+routers.get('/:id', controller.getSingleData)
 
 // DELETE single data (DELETE)
-routers.delete('/:id', (req, res) => {
-    res.json({msg: "Deleted the single data"})
-})
+routers.delete('/:id', controller.deleteData)
 
 // UPDATE single data (UPDATE)
-routers.patch('/:id', (req, res) => {
-    res.json({msg : "Data has been updated"})
-})
+routers.patch('/:id', controller.updateData)
 
 
 
