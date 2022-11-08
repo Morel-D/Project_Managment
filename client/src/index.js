72d342cdbd6ec63c138b5db7b1476f34e6d1a157
Morel-D <morelDen@gmail.com>
2022-11-08 12:28:16 -0800

    React useContext with auth

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecordContextProvider } from './context/recordContext';
+import { AuthContextProvider } from './context/authContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RecordContextProvider>
-       <App />
-    </RecordContextProvider>
+    <AuthContextProvider>
+       <RecordContextProvider>
+          <App />
+       </RecordContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
