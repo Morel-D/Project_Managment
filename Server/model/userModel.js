4d4cb877dbb1fafe538fbf921269694968753d62
Morel-D <morelDen@gmail.com>
2022-11-07 17:56:23 -0800

    Signup and password hashing

--- Server/model/userModel.js
+++ Server/model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 
 const authSchema = new Schema(
@@ -22,6 +23,29 @@ const authSchema = new Schema(
         }
     }
 )
+
+
+// staitic signup method
+
+
+authSchema.statics.signup = async function(userName, email, password)
+{
+    const exist = await this.findOne({ email })
+    
+    if (exist)
+    {
+        throw Error('Email already exist')
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ userName, email, password: hash })
+    
+    return user;
+
+}
+
 
 const authen = mongoose.model('User', authSchema);
 
